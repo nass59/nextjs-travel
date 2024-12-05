4ab47151b1f47b01ef06644d8423287e14e1e9f0
Nassim Afrete <nassim.afrete@gmail.com>
2024-12-05 19:53:41 +0100

    feat: add Storybook configuration and accordion component stories

---
+++ apps/storybook/.storybook/main.ts
@@ -0,0 +1,29 @@
+import type { StorybookConfig } from '@storybook/experimental-nextjs-vite';
+
+import { dirname, join } from 'node:path';
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
+
+const config: StorybookConfig = {
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: [
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-interactions'),
+  ],
+  framework: {
+    name: getAbsolutePath('@storybook/experimental-nextjs-vite'),
+    options: {},
+  },
+};
+
+export default config;
---
+++ apps/storybook/tailwind.config.ts
@@ -0,0 +1,2 @@
+// biome-ignore lint/performance/noBarrelFile: <explanation>
+export { config as default } from '@repo/tailwind-config';
