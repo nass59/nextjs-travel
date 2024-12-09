4be0c621ed52357f1c1b5f39a9ab5d564ee8553f
Nassim Afrete <nassim.afrete@gmail.com>
2024-12-09 22:11:50 +0100

    feat: add Collapsible component and corresponding Storybook stories

---
+++ packages/design-system/components/ui/collapsible.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+// biome-ignore lint/performance/noBarrelFile: <explanation>
+export {
+  CollapsibleContent,
+  CollapsibleTrigger,
+  Root as Collapsible,
+} from "@radix-ui/react-collapsible";
