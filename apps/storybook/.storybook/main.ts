fc88a714da394ccb34809a2ce82a7e41a259ad4c
Nassim Afrete <nassim.afrete@gmail.com>
2024-12-12 18:00:35 +0100

    refactor: standardize code formatting and improve readability across multiple files

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -6,9 +6,9 @@ import { dirname, join } from "node:path";
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): string {
+const getAbsolutePath = (value: string): string => {
   return dirname(require.resolve(join(value, "package.json")));
-}
+};
 
 const config: StorybookConfig = {
   stories: ["../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
