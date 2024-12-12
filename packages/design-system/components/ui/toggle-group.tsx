cdb2b32aee4fbd6b61121948bea1658d970b1b3d
Nassim Afrete <nassim.afrete@gmail.com>
2024-12-12 17:53:37 +0100

    feat: add Toggle and ToggleGroup components with Storybook stories for toggle functionality

---
+++ packages/design-system/components/ui/toggle-group.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Item, Root } from "@radix-ui/react-toggle-group";
+import type { VariantProps } from "class-variance-authority";
+import { type ComponentProps, createContext, useContext } from "react";
+
+import { toggleVariants } from "@repo/design-system/components/ui/toggle";
+import { cn } from "@repo/design-system/lib/utils";
+
+// Context
+const ToggleGroupContext = createContext<VariantProps<typeof toggleVariants>>({
+  size: "default",
+  variant: "default",
+});
+
+// ToggleGroup
+type ToggleGroupProps = ComponentProps<typeof Root> &
+  VariantProps<typeof toggleVariants>;
+
+const ToggleGroup = ({
+  className,
+  variant,
+  size,
+  children,
+  ref,
+  ...props
+}: ToggleGroupProps) => (
+  <Root
+    ref={ref}
+    className={cn("flex items-center justify-center gap-1", className)}
+    {...props}
+  >
+    <ToggleGroupContext value={{ variant, size }}>
+      {children}
+    </ToggleGroupContext>
+  </Root>
+);
+
+// ToggleGroupItem
+type ToggleGroupItemProps = ComponentProps<typeof Item> &
+  VariantProps<typeof toggleVariants>;
+
+const ToggleGroupItem = ({
+  className,
+  children,
+  variant,
+  size,
+  ref,
+  ...props
+}: ToggleGroupItemProps) => {
+  const context = useContext(ToggleGroupContext);
+
+  return (
+    <Item
+      ref={ref}
+      className={cn(
+        toggleVariants({
+          variant: context.variant || variant,
+          size: context.size || size,
+        }),
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Item>
+  );
+};
+
+// Exports
+export { ToggleGroup, ToggleGroupItem };
